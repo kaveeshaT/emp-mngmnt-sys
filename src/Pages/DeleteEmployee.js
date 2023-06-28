e3c20217e9d5253a17914b4ef162a05fac513b15
isuru356 <isuru356@gmail.com>
2023-06-29 00:45:36 +0530

    eight commit

--- src/Pages/DeleteEmployee.js
+++ src/Pages/DeleteEmployee.js
@@ -1,3 +1,70 @@
+// import React, { useState } from "react";
+// import axios from "axios";
+// import swal from "sweetalert";
+
+// const DeleteEmployee = () => {
+//   const [empNo, setEmpNo] = useState("");
+//   const [invalidInput, setInvalidInput] = useState(false);
+
+//   const handleDelete = async (e) => {
+//     e.preventDefault();
+
+//     // Show the confirmation dialog
+//     swal({
+//       title: "Are you sure?",
+//       text: "Once deleted, you will not be able to recover this imaginary file!",
+//       icon: "warning",
+//       buttons: true,
+//       dangerMode: true,
+//     }).then((willDelete) => {
+//       if (willDelete) {
+//         // User confirmed deletion
+//         const endpoint = `/api/v1.0/Employee/${empNo}`;
+//         axios
+//           .delete(endpoint)
+//           .then((response) => {
+//             swal("Success!", "Employee deleted successfully!", "success");
+//             setEmpNo("");
+//           })
+//           .catch((error) => {
+//             swal("Oops!", "Employee deletion unsuccessful!", "error");
+//             console.error(error);
+//             setEmpNo("");
+//           });
+//       } else {
+//         // User canceled deletion
+//         swal("Your imaginary file is safe!");
+//         setEmpNo("");
+//       }
+//     });
+//   };
+
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     if (empNo) {
+//       handleDelete(e);
+//     } else {
+//       setInvalidInput(true);
+//       swal("Oops!", "Enter Employee ID", "error");
+//     }
+//   };
+
+//   return (
+//     <div className="app">
+//       <form id="delteEmp">
+//         <input
+//           type="text"
+//           value={empNo}
+//           onChange={(e) => setEmpNo(e.target.value)}
+//           placeholder="Enter Employee Number"
+//         />
+//         <button onClick={handleClick}>Delete</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default DeleteEmployee;
 import React, { useState } from "react";
 import axios from "axios";
 import swal from "sweetalert";
@@ -9,6 +76,8 @@ const DeleteEmployee = () => {
   const handleDelete = async (e) => {
     e.preventDefault();
 
+    const endpoint = `/api/v1.0/Employee/${empNo}`;
+
     // Show the confirmation dialog
     swal({
       title: "Are you sure?",
@@ -19,7 +88,6 @@ const DeleteEmployee = () => {
     }).then((willDelete) => {
       if (willDelete) {
         // User confirmed deletion
-        const endpoint = `/api/v1.0/Employee/${empNo}`;
         axios
           .delete(endpoint)
           .then((response) => {
@@ -27,7 +95,7 @@ const DeleteEmployee = () => {
             setEmpNo("");
           })
           .catch((error) => {
-            swal("Oops!", "Employee deletion unsuccessful!", "error");
+            swal("Oops!", error.response.data.statusDescription, "error");
             console.error(error);
             setEmpNo("");
           });
